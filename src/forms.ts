4bf9344e33dc021450c11ac66717c6daa43c173f
Ben Kelly <Ben.Kelly@myknowledgemap.co.uk>
2018-07-05 08:40:14 +0100

    Change to form
    Changes to form fiels

--- src/forms.ts
+++ src/forms.ts
@@ -157,11 +157,11 @@ function sendToApi(
   // Build the request payload.
   const payload: ICreateActivityEvent = {
     activityId: props.activityId,
-    activityTime: response.getTimestamp().toUTCString(),
+    activityTime: props.activityTime,
     text1: props.text1,
     text2: form.getId(),
-    email: response.getRespondentEmail(),
-    userId: response.getRespondentEmail(),
+    email: props.email,
+    userId: props.userId(),
     int1: props.int1,
     int2: props.int2,
     date1: props.date1,
@@ -333,6 +333,8 @@ function showSettingsSidebar(): void {
     apiUrl: "",
     apiToken: "",
     activityId: "",
+	email = "",
+	userId="",
     text1: "",
     text2: "",
     int1: "",
